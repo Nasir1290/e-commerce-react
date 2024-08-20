282cd6b9f935cc1bf909592eb81ab963436752d3
Nasir1290 <nasirmia12901290@gmail.com>
2024-08-21 03:47:57 +0600

    initialize git

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 import {
   Navbar,
   NavbarBrand,
@@ -16,26 +17,28 @@ import {
   DropdownMenu,
   Avatar,
   Image,
-  Tab
+  Tab,
 } from "@nextui-org/react";
-import {SearchIcon} from "./SearchIcons.jsx"
-import LogoImage from "../../../assets/images/logo.png"
+import { SearchIcon } from "./SearchIcons.jsx";
+import LogoImage from "../../../assets/images/logo.png";
 import NavItem from "./NavItem.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function NavbarPage() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const navigate = useNavigate();
 
   const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
+    "Home",
+    "Shop",
+    // "Activity",
+    // "Analytics",
+    // "System",
+    // "Deployments",
+    // "My Settings",
+    // "Team Settings",
+    // "Help & Feedback",
+    "Missions",
   ];
 
   return (
@@ -46,19 +49,21 @@ export default function NavbarPage() {
           className="sm:hidden"
         />
         <NavbarBrand>
-          <Image className=" w-8 h-8 md:w-16 md:h-16 rounded-full" src={LogoImage}/>
-          <p className="md:font-bold font-semibold text-inherit">E-Shop</p>
+          <Image
+          onClick={() => {navigate("/shop")}}
+            className=" w-8 h-8 md:w-16 md:h-16 rounded-full"
+            src={LogoImage}
+          />
+          <p  onClick={() => {navigate("/shop")}} className="md:font-bold font-semibold text-inherit">E-Shop</p>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-
-
         {/* navigation items menu */}
-        <NavItem/>
+        <NavItem />
         {/* navigation items menu */}
-      
       </NavbarContent>
+
       <NavbarContent justify="end" className="items-center">
         {/* Search Input added here */}
         <Input
@@ -66,7 +71,8 @@ export default function NavbarPage() {
             base: "max-w-full sm:max-w-[10rem] h-10",
             mainWrapper: "h-full",
             input: "text-small",
-            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            inputWrapper:
+              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
           }}
           placeholder="Type to search..."
           size="sm"
@@ -92,7 +98,7 @@ export default function NavbarPage() {
               <p className="font-semibold">Signed in as</p>
               <p className="font-semibold">zoey@example.com</p>
             </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
+            {/* <DropdownItem key="settings">My Settings</DropdownItem> */}
             <DropdownItem key="team_settings">Team Settings</DropdownItem>
             <DropdownItem key="analytics">Analytics</DropdownItem>
             <DropdownItem key="system">System</DropdownItem>
@@ -106,20 +112,35 @@ export default function NavbarPage() {
       </NavbarContent>
 
       <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
+        {/* {menuItems.map((item, index) => ( */}
+        {/* <NavbarMenuItem key={`${item}-${index}`}> */}
+        <NavbarMenuItem>
+          <Link
+            color="primary"
+            className="w-full"
+            onClick={(event) => {
+              event.preventDefault();
+              navigate("/");
+              close;
+            }}
+            size="lg"
+          >
+            Home
+          </Link>
+          <Link
+            color="foreground"
+            onClick={(event) => {
+              event.preventDefault();
+              navigate("/shop");
+            }}
+            className="w-full"
+            size="lg"
+          >
+            Shop
+          </Link>
+        </NavbarMenuItem>
+
+        {/* ))} */}
       </NavbarMenu>
     </Navbar>
   );
